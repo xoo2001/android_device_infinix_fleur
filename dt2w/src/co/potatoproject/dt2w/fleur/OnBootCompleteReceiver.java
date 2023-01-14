962cc88f08b29420b0c7a20ba85a8f6d39110de6
nem1x <nisampitao@protonmail.com>
2023-01-14 15:27:41 +0000

    Rename remaining files and lines to fleur

--- dt2w/src/co/potatoproject/dt2w/fleur/OnBootCompleteReceiver.java
+++ dt2w/src/co/potatoproject/dt2w/fleur/OnBootCompleteReceiver.java
@@ -8,7 +8,7 @@ import android.os.UserHandle;
 
 public class OnBootCompleteReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
-        Intent sIntent = new Intent(context, DT2WServiceRosemary.class);
+        Intent sIntent = new Intent(context, DT2WServicefleur.class);
         context.startServiceAsUser(sIntent, UserHandle.CURRENT);
     }
 }
