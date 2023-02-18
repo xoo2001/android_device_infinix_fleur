e82211d961a9a43dcfaa882da1e9baa33d9ba820
Nem1x <90353607+Nem1xx@users.noreply.github.com>
2023-02-18 21:39:38 +0100

    fleur: change Build prop and fingerprint to Redmi and global

--- lineage_fleur.mk
+++ lineage_fleur.mk
@@ -17,7 +17,8 @@ PRODUCT_MODEL := fleur
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := fleur
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="fleur-user 12 SP1A.210812.016 V13.0.7.0.SKEMIXM release-keys"
 
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := POCO/fleur_p_global/fleur:12/SP1A.210812.016/V13.0.7.0.SKEMIXM:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="fleur-user 12 SP1A.210812.016 V13.0.7.0.SKEMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/fleur/fleur:12/SP1A.210812.016/V13.0.7.0.SKEMIXM:user/release-keys
