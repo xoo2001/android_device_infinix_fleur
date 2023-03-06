c328b958297b474c9ea4ec76cf572285a0523fdb
Nem1xx <nemanjanedeljkovic738@gmail.com>
2023-03-06 18:31:48 +0100

    fleur: bind vendor overlay and define dt2w node
    Former-commit-id: 64d5003b747ef8e4d43999904b2f167657ed8c3a

--- Android.mk
+++ Android.mk
@@ -1,7 +1,21 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 LOCAL_PATH := $(call my-dir)
+
 ifeq ($(TARGET_DEVICE),fleur)
-include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
 include $(CLEAR_VARS)
+
+LIGHT_REPLACEMENT += $(TARGET_OUT_PRODUCT)/vendor_overlay/${PRODUCT_TARGET_VNDK_VERSION}/bin/hw/android.hardware.lights-service.mediatek
+$(LIGHT_REPLACEMENT): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -s /system/bin/hw/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(LIGHT_REPLACEMENT)
 endif
 
 ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
