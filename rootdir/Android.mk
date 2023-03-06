eba50e5cb0775a1e0e5a0161059b1c7b4f86a025
Nem1xx <nemanjanedeljkovic738@gmail.com>
2023-03-06 18:37:47 +0100

    fleur: rootdir: introduce init.mediatek.rc

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -21,3 +21,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.mt6781
 LOCAL_MODULE_PATH  := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_EXTRA_VNDK_VERSIONS)/etc
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.mediatek.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.mediatek.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)
