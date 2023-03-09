67b515c1d5ffe774709d70c9c63723a27f32e799
Nem1xx <nemanjanedeljkovic738@gmail.com>
2023-03-09 21:36:09 +0000

    fleur: BoardConfig: Fix Architecture and include Firmware in ROM
    Former-commit-id: 9f1eefc60bf3ea0d511d0d8b0b71952475ba9332

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,18 +8,18 @@ DEVICE_PATH := device/xiaomi/fleur
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 := 
-TARGET_CPU_VARIANT := generic
-TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a76
+TARGET_CPU_VARIANT_RUNTIME := cortex-a76
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
-TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
+TARGET_2ND_CPU_VARIANT := cortex-a55
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
 # APEX
 DEXPREOPT_GENERATE_APEX_IMAGE := true
@@ -41,6 +41,9 @@ TARGET_TAP_TO_WAKE_NODE := "proc/tp_gesture"
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
+
+# Firmware
+-include vendor/xiaomi/fleur-firmware/BoardConfigVendor.mk
 
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 2
