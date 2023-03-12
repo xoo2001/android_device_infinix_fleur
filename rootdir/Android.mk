90a0559d0d82247a10bc45cad9e7bc5bdbf12b36
Nem1xx <nemanjanedeljkovic738@gmail.com>
2023-03-12 14:36:28 +0000

    fleur: Add init.mediatek.rc to rootdir
    Former-commit-id: 734bcc59f1c1923d7a7b06c0d18ebe4d487ac709

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -15,6 +15,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.mediatek.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.mediatek.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.mt6781
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
