39e04c3fa3d6dba9f93c31b305026ed507963d6d
Nem1xx <nemanjanedeljkovic738@gmail.com>
2023-03-18 11:47:05 +0000

    fleur: Move audio config to /configs

--- device.mk
+++ device.mk
@@ -52,7 +52,7 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_configuration.xml
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_configuration.xml
 
 #APN config
 PRODUCT_COPY_FILES += \
