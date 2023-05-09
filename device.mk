74bd852088ba4404a562c18f68e20b6cb636a6f7
Albert I <kras@raphielgang.org>
2023-05-09 07:05:35 +0000

    fleur: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Signed-off-by: priiii1808 <priyanshusinghal0818@gmail.com>
    Signed-off-by: ShazuxD <shazuxd7@gmail.com>

--- device.mk
+++ device.mk
@@ -58,6 +58,10 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
 
+# Android GO
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Bootctrl
 PRODUCT_PACKAGES += \
     bootctrl.default \
