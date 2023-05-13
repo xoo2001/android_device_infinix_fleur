89ddba337d39438edc78c33a774209d456fcd21c
Nem1xx <nemanjanedeljkovic738@gmail.com>
2023-05-13 14:07:13 +0000

    fleur: prebuilts: Patch kernel binary and fix bpf on battery saver
    Thanks to R0rt1z2, we are able to patch kernel using mtk-bpf-patcher and fix bpf bugs on battery saver

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,7 +59,7 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 
 # Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilts/kernel
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilts/Image.gz
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilts/dtb.img
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilts/dtbo.img
 
