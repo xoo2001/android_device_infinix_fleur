df2f89b067524023e4a9fa2d296b58cbdc061580
Nem1xx <nemanjanedeljkovic738@gmail.com>
2023-05-18 13:11:47 +0000

    fleur: rootdir: Stop building init.mt6781.rc
    Former-commit-id: 312fe7d9b40d307dc5c43d70d3a29a827e7d5df9

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,14 +7,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.mt6781.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.mt6781.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mediatek.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
