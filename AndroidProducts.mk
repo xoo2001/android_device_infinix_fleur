330f4b0f280c2e40bdc3bbf98d0e9de77d61b66a
ShazuxD <shazuxd7@gmail.com>
2023-06-22 15:23:17 +0530

    fleur: derp-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_fleur.mk
+    $(LOCAL_DIR)/derp_fleur.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_fleur-user \
-    aosp_fleur-userdebug \
-    aosp_fleur-eng
+    derp_fleur-user \
+    derp_fleur-userdebug \
+    derp_fleur-eng
--- derp_fleur.mk
+++ derp_fleur.mk
@@ -2,15 +2,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Derpfest stuff
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit from fleur device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := aosp_fleur
+PRODUCT_NAME := derp_fleur
 PRODUCT_MODEL := Redmi Note 11S 4G
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_BOOT_ANIMATION_RES := 1080
