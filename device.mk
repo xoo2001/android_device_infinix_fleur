40f2837aa3ccebd6c8e9a4f3791137a334f5c491
ShazuxD <shazuxd7@gmail.com>
2023-07-09 09:35:54 +0530

    fleur: rro_overlays: Set default Settings.Global.DEVICE_NAME
    * MIUI uses ro.boot.product.vendor.sku for sku devices
    - we have fleur,fleurp,miel,mielp
    
    * Reference:
       https://github.com/AdarshGrewal/android_device_xiaomi_sweet/commit/a4524e8d6648ec8e7f98e7d779d11eb98a0f5103
       https://github.com/AOSPK-Devices/device_xiaomi_surya/commit/52de6b56fcf560a02c5ced8cde49b2186037da18
    
    Signed-off-by: ShazuxD <shazuxd7@gmail.com>
    
    Former-commit-id: 5c09599891cb13f4be0dc439e2cee8acc523a40a

--- device.mk
+++ device.mk
@@ -158,6 +158,13 @@ PRODUCT_PACKAGES += \
     SettingsOverlayFleur \
     SimpleDeviceConfigOverlayfleur
 
+# Runtine Device Name Overlays
+PRODUCT_PACKAGES += \
+    FleurSettingsProviderOverlay \
+    FleurpSettingsProviderOverlay \
+    MielSettingsProviderOverlay \
+    MielpSettingsProviderOverlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
