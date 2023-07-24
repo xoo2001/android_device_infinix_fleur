6727c367d1450940c31d038b648696bc84a2b6a1
FPSensor <gkartyt@gmail.com>
2023-07-24 01:20:15 -0300

    Fleur: fix incorrect call recording package declaration

--- device.mk
+++ device.mk
@@ -149,8 +149,7 @@ PRODUCT_PACKAGES += \
 
 # Call recording
 PRODUCT_PACKAGES += \
-
-com.google.android.apps.dialer.call_recording_audio.features.xml
+    com.google.android.apps.dialer.call_recording_audio.features.xml
 
 # NFC
 PRODUCT_PACKAGES += \
