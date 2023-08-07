5c76bc0d9252db646f95f0c9041283a71091bdd5
Rohit Tiwari <130814475+RT1648@users.noreply.github.com>
2023-08-07 22:30:34 +0530

    Fleur: superior_fleur.mk: Fix Errors

--- superior_fleur.mk
+++ superior_fleur.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common SuperiorOS stuff
-$(call inherit-product, vendor/superior/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit from fleur device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
