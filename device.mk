2837a73d7428586b9bcd9eeae33bb03ef6dd93a4
Nem1xx <nemanjanedeljkovic738@gmail.com>
2023-08-09 10:51:27 +0000

    fleur: rro_overlays: Set default Settings.Global.DEVICE_NAME
    * MIUI uses ro.boot.product.vendor.sku for sku devices
    - we have fleur,fleurp,miel,mielp
    
    * Reference:
       https://github.com/AdarshGrewal/android_device_xiaomi_sweet/commit/a4524e8d6648ec8e7f98e7d779d11eb98a0f5103
       https://github.com/AOSPK-Devices/device_xiaomi_surya/commit/52de6b56fcf560a02c5ced8cde49b2186037da18
    
    Signed-off-by: ShazuxD <shazuxd7@gmail.com>
    
    Former-commit-id: 5c09599891cb13f4be0dc439e2cee8acc523a40a

--- device.mk
+++ device.mk
@@ -152,10 +152,17 @@ PRODUCT_PACKAGES += \
     SystemUIOverlayFleur \
     SettingsOverlayFleur
 
+# Runtine Device Name Overlays
+PRODUCT_PACKAGES += \
+    FleurSettingsProviderOverlay \
+    FleurpSettingsProviderOverlay \
+    MielSettingsProviderOverlay \
+    MielpSettingsProviderOverlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
- 
+
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.mt6781:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.mt6781
