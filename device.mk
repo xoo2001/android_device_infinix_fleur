bc6a0c9188e74aaeb3800870917eb49b7e719505
Rohit Tiwari <rohitttiwari52@gmail.com>
2023-09-22 09:12:30 +0530

    Fleur: Drop `XiaomiParts`
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- device.mk
+++ device.mk
@@ -182,7 +182,7 @@ PRODUCT_PACKAGES += \
     FleurSettingsProviderOverlay \
     FleurpSettingsProviderOverlay \
     MielSettingsProviderOverlay \
-    MielpSettingsProviderOverlay
+    MielpSettingsProviderOverlay \
     SettingsLibOverlayFleur \
     SimpleDeviceConfigOverlayFleur
 
@@ -206,9 +206,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
         ro.debuggable=1 \
         persist.service.adb.enable=1 \
         persist.sys.usb.config=adb,mtp
-# Parts
-PRODUCT_PACKAGES += \
-    XiaomiParts
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
