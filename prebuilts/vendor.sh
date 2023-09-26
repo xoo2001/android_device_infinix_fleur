5826015519e62e82559ada05a152e5d2bbac2d30
Rohit Tiwari <rohitttiwari52@gmail.com>
2023-09-26 21:04:35 +0530

    fleur: Improve `vendor.sh` script logic
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- prebuilts/vendor.sh
+++ prebuilts/vendor.sh
@@ -1,4 +1,5 @@
 echo "Downloading Prebuilt Vendor, Please Wait A Few Minutes"
 cd device/xiaomi/fleur/prebuilts || echo "Make Sure You Are Running This Script Only From Root Directory Of Rom Source"
-wget https://github.com/RT1648/Extras/releases/download/13.0.9.0/vendor.img
+sudo apt install aria2c || sudo apt-get aria2c
+aria2c https://github.com/RT1648/Extras/releases/download/13.0.9.0/vendor.img
 echo "Download Completed, Now You Can Build"
