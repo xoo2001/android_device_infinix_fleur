e4916bf52e099d1dc7c7b69723ab6bce5dfd990b
RT1648 <rohitttiwari52@gmail.com>
2023-09-29 16:28:53 +0530

    fleur: prebuilts: fix command not found error

--- prebuilts/vendor.sh
+++ prebuilts/vendor.sh
@@ -1,5 +1,5 @@
 echo "Downloading Prebuilt Vendor, Please Wait A Few Minutes"
 cd device/xiaomi/fleur/prebuilts || echo "Make Sure You Are Running This Script Only From Root Directory Of Rom Source"
-sudo apt install aria2c || sudo apt-get aria2c
+sudo apt install -y aria2
 aria2c https://github.com/RT1648/Extras/releases/download/13.0.9.0/vendor.img
 echo "Download Completed, Now You Can Build"
